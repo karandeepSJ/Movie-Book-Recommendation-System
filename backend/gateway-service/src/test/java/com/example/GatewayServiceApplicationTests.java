5ddf9dace19871e17e613ff42394b7bf4652924a
Aditya Arora <adityarora97@gmail.com>
2020-04-26 14:46:30 +0530

    Added gateway service and updated API routes

--- backend/gateway-service/src/test/java/com/example/GatewayServiceApplicationTests.java
+++ backend/gateway-service/src/test/java/com/example/GatewayServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MovieServiceApplicationTests {
+public class GatewayServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
