772f17109d9c662e3f80f6d912fab6f09afa3a82
Karan <karan.jkps@gmail.com>
2020-04-26 18:02:36 +0530

    Cleaned frontend code, set up React router

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import SearchPage from "./components/SearchPage/SearchPage";
+import Dashboard from "./components/Dashboard/Dashboard";
+import MoviePage from "./components/ItemPage/MoviePage";
+import BookPage from "./components/ItemPage/BookPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Dashboard} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/movie/:id" component={MoviePage} />
+          <Route path="/book/:isbn" component={BookPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/my-app/src/components/ItemPage/BookPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+class BookPage extends Component {
+  render() {
+  	return ""
+  	
+  }
+}
+export default BookPage;
