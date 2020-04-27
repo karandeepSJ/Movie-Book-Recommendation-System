8d471a361b9e18759ef2c14daf822113e8e70c1e
aniketshrimal <aniketshrimal98@gmail.com>
2020-04-28 00:19:39 +0530

    changes in Recommendations

--- frontend/my-app/src/components/Dashboard/Dashboard.js
+++ frontend/my-app/src/components/Dashboard/Dashboard.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Recommendation from '../Recommendation/Recommendation'
 
 class Dashboard extends Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 	    this.state = {
 	    	loggedIn: false,
+	    	loading:true,
+	    	movieDbId: null,
+	    	bookDbId: null,
 	    };
 	}
 
@@ -12,34 +16,27 @@ class Dashboard extends Component {
 		if (localStorage.getItem("user"))
 		{
 			const user = JSON.parse(localStorage.getItem("user"));
-			this.setState({movieDBId: user.movieDBId, bookDBId: user.bookDBId, loggedIn:true})
+			this.setState({movieDbId: user.movieDbId, bookDbId: user.bookDbId, loggedIn:true, loading:false})
+
 		}
 		else
 		{
-			this.setState({loggedIn:false})
+			this.setState({loggedIn:false,loading:false})
 		}
-		// const movieId = this.props.match.params.id
-		// axios.get(`http://localhost:5050/api/m/movies/details/${movieId}`)
-		// .then (response => {
-		// 	if(!response.data)
-		// 	{
-		// 		this.props.history.push('/error')
-		// 	}
-		// 	else	
-		// 	{
-		// 		this.setState(response.data);
-		// 		this.setState({validMovie: true});
-		// 	}
-		// })
-		// .catch(err => {
-		// 	console.log(err);
-		// })
+		
 	}
 
 	renderLoggedIn() {
+		var user = null
+		if (localStorage.getItem("user"))
+		{
+		 	user = JSON.parse(localStorage.getItem("user"));
+
+		}
 		return (
 			<div>
   			<h4 className="pt-5 h4-responsive">Recommended Movies:</h4>
+  			<Recommendation movieDbId={user.movieDbId} bookDbId={user.bookDbId} />
   			<h4 className="pt-5 h4-responsive">Recommended Books:</h4>
 			</div>
   	) 
@@ -54,7 +51,11 @@ class Dashboard extends Component {
 	}
 
   render() {
-  	var loggedIn = this.state.loggedIn; 
+  	var loggedIn = this.state.loggedIn;
+  	if(this.state.loading) {
+          return 'Loading...'
+    } 
+    
   	return (
   		<div className="container">
   		{loggedIn && this.renderLoggedIn()}
