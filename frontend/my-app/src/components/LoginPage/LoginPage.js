3e7d1cf8d9d766a1dc368e9e7af24c94d8e07845
sivangisingh <sivangi.singh@students.iiit.ac.in>
2020-04-27 14:00:55 +0530

    Loginpage updated

--- frontend/my-app/src/components/LoginPage/LoginPage.js
+++ frontend/my-app/src/components/LoginPage/LoginPage.js
@@ -1,21 +1,32 @@
 import React from "react";
+import VerifyLogin from "./VerifyLogin";
+import "../../style.css"
 
 export default class LoginPage extends React.Component {
-constructor() {
-    super();
+constructor(props) {
+    super(props);
     this.state = {
       username: '',
       password: '',
       error: '',
-      toggle: false,
+      submit: false,
+      name: '',
+      failure: ''
     };
-
     this.handlePassChange = this.handlePassChange.bind(this);
     this.handleUserChange = this.handleUserChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.dismissError = this.dismissError.bind(this);
+    this.againsubmit = this.againsubmit.bind(this);
   }
 
+  againsubmit(){
+    this.setState({submit:false});
+    this.setState({failure:'fail'});
+  }
+  switch(){
+    this.setState({failure:''});
+  }
   dismissError() {
     this.setState({ error: '' });
   }
@@ -30,7 +41,7 @@ constructor() {
     if (!this.state.password) {
       return this.setState({ error: 'Password is required' });
     }
-
+    this.setState({submit:true})
     return this.setState({ error: '' });
   }
 
@@ -47,70 +58,38 @@ constructor() {
   }
 
   render() {
-    // NOTE: I use data-attributes for easier E2E testing
-    // but you don't need to target those (any css-selector will work)
-    // if(!this.props.show){
-    //       return null;
-    //   }
 
     return (
       <div className="login">
         <form onSubmit={this.handleSubmit}>
-        <div className={this.state.toggle ? "login login-closed" : ""} >
-
-          <h1>Log In</h1>
-          <label>User Name</label>
           <br/>
-          <input className="input" type="text" data-test="username" value={this.state.username} onChange={this.handleUserChange} />
+          <label>Email</label>
+          <br/>
+          <input className="input" type="text" data-test="username" value={this.state.username} onChange={this.handleUserChange}/>
           <br/>
           <label>Password</label>
           <br/>
           <input className="input" type="password" data-test="password" value={this.state.password} onChange={this.handlePassChange} />
-
           <input className="submit-button" type="submit" value="Log In" data-test="submit"/>
-          <p className="in-out"> 
-            Don't have an account? {" "}
-            <a href="#" onClick={()=>this.setState({
-              toggle: true
-            })}>Sign Up Here</a>
-          </p>
-          {
-            this.state.error &&
-            <h4 data-test="error" onClick={this.dismissError}>
-              <button className="button-error" onClick={this.dismissError}>✖</button>
-              {this.state.error}
-            </h4>
-          }
-        </div>
-        <div className={!this.state.toggle ? "sign-up sign-up-closed" : ""}> 
-          <h1>Sign Up</h1>
-          <label>User Name</label>
-          <br/>
-          <input className="input" type="text" data-test="username" value={this.state.username} onChange={this.handleUserChange} />
-          <br/>
-          <label>Password</label>
-          <br/>
-          <input className="input" type="password" data-test="password" value={this.state.password} onChange={this.handlePassChange} />
-
-          <input className="submit-button" type="submit" value="Sign Up" data-test="submit"/>
-          
-          <p className="in-out"> 
-            Login after Sign up{" "}
-            <a href="#" onClick={()=>this.setState({
-              toggle: false
-            })}>Log In Here</a>
-          </p>
-          {
-            this.state.error &&
-            <h4 data-test="error" onClick={this.dismissError}>
-              <button className="button-error" onClick={this.dismissError}>✖</button>
-              {this.state.error}
-            </h4>
-          }
-        </div>
         </form>
+          <br/>
+          {
+            this.state.error &&
+            <h6 data-test="error" onClick={this.dismissError}>
+              <button className="button-error" onClick={this.dismissError}>✖</button>
+              {this.state.error}
+            </h6>
+          }
+          {
+            this.state.failure && 
+            <h6>Incorrect Id or Password{this.state.switch}</h6>
+          }
+          <br/>
+          {
+            this.state.submit &&
+            <VerifyLogin email={this.state.username} password={this.state.password} modalclose={this.props.modalclose} changename={this.props.changename} againsubmit={this.againsubmit}/>
+          }
       </div>
-
     );
   }
 }
