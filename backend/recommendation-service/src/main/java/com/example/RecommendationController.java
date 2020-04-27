a8fb44872f68c86b1421d1313d5bc764723ef8b9
Aditya Arora <adityarora97@gmail.com>
2020-04-27 16:48:03 +0530

    added cross origin in recommendation API

--- backend/recommendation-service/src/main/java/com/example/RecommendationController.java
+++ backend/recommendation-service/src/main/java/com/example/RecommendationController.java
@@ -3,11 +3,13 @@ package com.example;
 import com.mongodb.DBObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/recommend")
 public class RecommendationController {
